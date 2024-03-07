55c364369398908b34af8ebbb3a09ee693d5155f
waterismm <tobiaszpopr7@gmail.com>
2024-03-07 16:25:02 +0000

    Database Access + Live Editing
    Modified docker file + docker-compose.yml to allow for live editing of the files and updated displays on localhost; database is having issues with being accessed due to lack of permission.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql2');
 
 // init app
 const app = express();
-
+const port = 5000;
 
 // load view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// home route
-app.get('/', (req, res) => {
-    res.render('index');
+// Server assets from 'static' folder
+app.use(express.static("static"));
+
+/* Setup database connection */
+const db = mysql.createConnection({
+  host: process.env.DATABASE_HOST || "localhost",
+  user: "user",
+  password: "password",
+  database: "world",
 });
 
+// home route
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/ping", (req, res) => {
+    res.send("pong");
+  });
+
+  // Returns an array of cities from the database
+app.get("/cities", (req, res) => {
+    db.query("SELECT * FROM `city`", (err, rows, fields) => {
+      console.log(`/cities: ${rows.length} rows`);
+      return res.send(rows);
+    });
+  });
+
 // start server
-const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`app listening on http://localhost:${port}`)
+  console.log(`Server running on port ${port}`);
 });
