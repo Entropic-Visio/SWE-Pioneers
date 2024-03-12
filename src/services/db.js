ec23f80b0b59cf07e8e5bf054840e3c5a50eebce
Abdullahi Roge <rogea@roehampton.ac.uk>
2024-03-12 11:22:47 +0000

    made some few clarifications

--- src/services/db.js
+++ src/services/db.js
@@ -1,4 +1,6 @@
+//Import the mysql2 package to work with MySQL databases in a Node.js environment.
 const mysql = require('mysql2');
+// Import the database configuration from the 'configs' directory. This file contains the database connection settings
 const dbConfig = require('../configs/dbConfigs.js');
 
 const connection = mysql.createConnection({
@@ -8,4 +10,6 @@ const connection = mysql.createConnection({
     database: dbConfig.DATABASE,
 });
 
+// Export the connection object so it can be used elsewhere in the application. 
+// This allows other parts of the application to perform database operations using this single connection.
 module.exports = connection;
