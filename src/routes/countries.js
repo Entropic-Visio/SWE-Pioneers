620f34747eaff8822926dc98f089bc435cd234dc
Entropica <entropicvisio@gmail.com>
2024-03-12 10:24:38 +0000

    Fixed countries.js issue where it had the wrong path to get services.

--- src/routes/countries.js
+++ src/routes/countries.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router()
 const mysql = require('mysql2');
-const db = require('../models/db.js');
+const db = require('../services/db.js');
 
 router.get("/", (req, res) => {
     db.query("SELECT * FROM `country`", (err, rows, fields) => {
