492201f15a1657825c8f99ce34a640de8b2020aa
Neo <ndlebezikhanyilanga@gmail.com>
2024-03-25 19:53:11 +0000

    Added a few things

--- src/routes/countries.js
+++ src/routes/countries.js
@@ -7,12 +7,14 @@ const db = require('../services/db.js');
 router.get("/", async (req, res) => {
   try {
     const [rows, fields] = await db.query("SELECT * FROM `country`");
-    console.log(`/cities: ${rows.length} rows`);
-    return res.send(rows);
+    console.log(`/countries: ${rows.length} rows`);
+    return res.render("country");
   } catch (error) {
     console.error(error);
     return res.status(500).render("500");
   }
+
+
 });
 
 module.exports = router;
