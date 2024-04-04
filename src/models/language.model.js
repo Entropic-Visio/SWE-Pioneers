6ddf639da5140fa4574d79f3b668fd85c5d14993
Neo <ndlebezikhanyilanga@gmail.com>
2024-04-04 19:22:58 +0100

    type error when trying to get population

--- src/models/language.model.js
+++ src/models/language.model.js
@@ -1,11 +1,11 @@
 const countryLanguageServices = require('../services/countryLanguage.services.js');
-
 class Language {
     constructor(countryCodeAlpha3) {
         this.countryCodeAlpha3 = countryCodeAlpha3.toUpperCase();
         this.language = null;
         this.isOfficial = null;
         this.percentage = null;
+        this.population = null;
     }
 
     async getLanguageInformation() {
@@ -14,4 +14,5 @@ class Language {
             const sql = "SELECT * FROM `countryLanguage` WHERE Code = ?";
         }
     }
+
 }
--- src/routes/language.js
+++ src/routes/language.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const countryLanguageServices = require('../services/countryLanguage.services.js');
+const countryModel = require('../models/country.model.js')
 
 router.get("/", async (req, res) => {
     try{ 
         const results = await countryLanguageServices.getAllCountryLanguages();
-        return res.render("language", { countryLanguages: results});
+        const langpop = await countryModel.getCountryPopulation()
+        return res.render("language", { countryLanguages: results });
     } catch (error){
         console.error(error);
         return res.status(500).render("500");
     }
+
+    
 });
 
 module.exports = router;
