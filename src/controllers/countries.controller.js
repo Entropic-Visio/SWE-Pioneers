7bddf0b72d7d2ceb576712dd48b817d8abab1aca
Neo <ndlebezikhanyilanga@gmail.com>
2024-04-17 12:49:33 +0100

    Countries Tables

--- src/controllers/countries.controller.js
+++ src/controllers/countries.controller.js
@@ -1,8 +1,15 @@
 const isUserLoggedIn = require('../middlewares/isUserLoggedIn.middleware.js');
+const { getAllCountries } = require('../services/countries.service.js');
 
-const GetCountriesView = (req, res) => {
-    const isLoggedIn = isUserLoggedIn(req);
-    return res.render('countries/countries.view.pug', { isLoggedIn, user: req.session.user });
+const GetCountriesView = async (req, res) => {
+    try {
+        const isLoggedIn = isUserLoggedIn(req);
+        const results = await getAllCountries();
+        return res.render('countries/countries.view.pug', { isLoggedIn, user: req.session.user, countries: results });
+    } catch (error) {
+        console.error("Error Fetching Countries", error.message);
+        return error
+    }
+    
 }
-
 module.exports = { GetCountriesView }
---
+++ src/models/country.model.js
@@ -0,0 +1,39 @@
+const countryServices = require('../services/countries.service.js')
+
+class Country {
+    constructor(id) {
+        this.code = countryCode;
+        this.name = null;
+        this.contient = null;
+        this.region = null;
+        this.population = null;
+        this.captial = null;
+    };
+
+    async getCountryInformation() {
+        if (typeof this.name !== "string") {
+                const result = await countryServices.getCountryByCode(this.code);
+                if (result) { 
+                    this.name = result.Name;
+                    this.contient = result.CountryCode;
+                    this.region = result.Region
+                    this.population = parseInt(result.Population);
+                    this.captial = result.Captial
+                } else {
+                    throw new Error("City not found");
+                }
+        }
+    };
+
+    async getCountryName() {
+        if (typeof this.name !== "string") {
+            const result = await countryServices.getCountryByCode(this.code);
+            if (result) {
+                this.name = result.Name;
+                return this.name
+            }
+        }
+    };
+};
+
+module.exports = { Country };
