242be4efebc3c8ceb4bbc8bb6e2205f23c01a426
Neo Msika (Student) <msikan@roehampton.ac.uk>
2024-04-22 15:17:15 +0100

    Population started

--- src/controllers/population.controller.js
+++ src/controllers/population.controller.js
@@ -2,7 +2,7 @@ const isUserLoggedIn = require('../middlewares/isUserLoggedIn.middleware.js');
 
 const GetPopulationView = (req, res) => {
     const isLoggedIn = isUserLoggedIn(req);
-    return res.render('population/population.report.pug', { isLoggedIn, user: req.session.user });
+    return res.render('population/population.view.pug', { isLoggedIn, user: req.session.user });
 }
 
 module.exports = { GetPopulationView }
