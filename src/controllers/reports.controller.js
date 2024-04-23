354ec02537b67f003f69d7b5fbec0693b8fc258d
Entropica-Caelum <entropicacaelum@gmail.com>
2024-04-23 12:35:14 +0100

    Compelted World Report

--- src/controllers/reports.controller.js
+++ src/controllers/reports.controller.js
@@ -1,8 +1,21 @@
 const isUserLoggedIn = require('../middlewares/isUserLoggedIn.middleware.js');
+const countriesService = require('../services/countries.service.js');
 
 const GetReportsView = (req, res) => {
     const isLoggedIn = isUserLoggedIn(req);
     return res.render('reports/reports.view.pug', { isLoggedIn, user: req.session.user });
 }
 
-module.exports = { GetReportsView }
+const GetWorldPopulationReportView = async (req, res) => {
+    const isLoggedIn = isUserLoggedIn(req);
+    const result = await countriesService.getWorldPopulation();
+    let worldPopulation = 0
+
+    for (let i = 0; i < result.length; i++) {
+        worldPopulation += result[i].Population;
+    }
+
+    return res.render('reports/worldPopulation.report.pug', { isLoggedIn, user: req.session.user, worldPopulation });
+}
+
+module.exports = { GetReportsView, GetWorldPopulationReportView }
--- src/services/countries.service.js
+++ src/services/countries.service.js
@@ -49,7 +49,33 @@ async function getCountryByCode(countryCode) {
     }
 };
 
+/**
+ * Retrieves a city from the database based on the provided city ID.
+ * @param {string} countryCode - The ID of the city to retrieve.
+ * @returns {Promise<Object>} A promise that resolves to the city object if found, otherwise rejects with an error.
+ */
+async function getWorldPopulation() { 
+    // SQL query to select a country from the `country` table based on the given code
+    const sql = "SELECT Population FROM `country`";
+
+    try {
+        // Execute the SQL query asynchronously, passing the country code as a parameter
+        const [rows, fields] = await worldConnection.query(sql);
+
+        // Return the first country object from the result set
+        return rows;
+    } catch (error) {
+        // Handle errors that occur during database query execution
+        console.error("Error Fetching Country by Code: ", error);
+        
+        // Throw a new error if country retrieval fails
+        throw new Error("Failed to Fetch Country by code");
+    }
+};
+
+
 module.exports = { 
     getAllCountries, 
     getCountryByCode,
+    getWorldPopulation,
 };
